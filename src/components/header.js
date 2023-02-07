dc8357e9100105e62436078bcd8fcfa2e9911bdb
Vishal Behal <vikkbehal@Vishals-Laptop.local>
2023-02-07 14:40:10 +0530

    updated ui

--- src/components/header.js
+++ src/components/header.js
@@ -3,22 +3,22 @@ import React from 'react'
 const Header = () => {
   return (
     <header id="header">
-    <div className='logo'>
-        <p>Vishal Behal</p>
-    </div>
-    <div>
-        <ul>
-            <li>
-                my work.
-            </li>
-            <li>
-                about me.
-            </li>
-            <li>
-                reads.
-            </li>
-        </ul>
-    </div>
+        <div className='logo'>
+            <p>Vishal Behal</p>
+        </div>
+        <div>
+            <ul>
+                <li>
+                    my work.
+                </li>
+                <li>
+                    about me.
+                </li>
+                <li>
+                    reads.
+                </li>
+            </ul>
+        </div>
     </header>
   )
 }
---
+++ src/components/selectedWork.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const WorkCard = () => {
+    return (
+      <>
+      <p>kjfhkde</p>
+      </>
+    )
+  }
+
+const SelectedWork = () => {
+  return (
+    <section id='selectedWork' >
+        <p className='title'>Selected Work</p>
+        <WorkCard/>
+    </section>
+  )
+}
+
+export default SelectedWork;
