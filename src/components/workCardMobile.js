9e858d1f2939d5e1af4fec6ca49bf0d849add870
Vishal Behal <vikkbehal@live.com>
2023-04-04 19:34:30 +0530

    mobile issue solve

--- src/components/workCardMobile.js
+++ src/components/workCardMobile.js
@@ -1,16 +1,17 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 
 const WorkCardMobile = (props) => {
-    return (
-      <motion.div
+  return (
+    <motion.div
       initial={{ opacity: 0, y: 10 }}
       whileInView={{ opacity: 1, y: 0 }}
-      transition={{ 
+      transition={{
         duration: 0.5,
-        delay:0
-         }}>
+        delay: 0,
+      }}
+    >
       <div
         className="workCard"
         style={{
@@ -26,21 +27,25 @@ const WorkCardMobile = (props) => {
           <p className="sno">● 00{props.sno}</p>
           <p className="title">{props.title}</p>
           <p className="desc">{props.desc}</p>
-  
+
           <ul className="taglist">
             {props.tags &&
               props.tags.map((singleTag, i) => (
-                <li key={`uitags-${props.sno}--${i}`} className="product-tag tag">
+                <li
+                  key={`uitags-${props.sno}--${i}`}
+                  className="product-tag tag"
+                >
                   {singleTag}
                 </li>
               ))}
           </ul>
-  
-          <Link target="_blank" to={props.ctaLink} className="ctaButton" >
+
+          <Link to={props.ctaLink} className="ctaButton">
             {props.linkText}
           </Link>
         </div>
-      </div><div
+      </div>
+      <div
         className="workCard"
         style={{
           backgroundImage: `url(${props.bg})`,
@@ -55,24 +60,26 @@ const WorkCardMobile = (props) => {
           <p className="sno">● 00{props.sno}</p>
           <p className="title">{props.title}</p>
           <p className="desc">{props.desc}</p>
-  
+
           <ul className="taglist">
             {props.tags &&
               props.tags.map((singleTag, i) => (
-                <li key={`uitags-${props.sno}--${i}`} className="product-tag tag">
+                <li
+                  key={`uitags-${props.sno}--${i}`}
+                  className="product-tag tag"
+                >
                   {singleTag}
                 </li>
               ))}
           </ul>
-  
-          <Link target="_blank" to={props.ctaLink} className="ctaButton" >
+
+          <Link to={props.ctaLink} className="ctaButton">
             {props.linkText}
           </Link>
         </div>
       </div>
-         </motion.div>
-    );
-  };
-
+    </motion.div>
+  );
+};
 
 export default WorkCardMobile;
